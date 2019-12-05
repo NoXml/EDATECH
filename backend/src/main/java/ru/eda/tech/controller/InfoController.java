bf94e5ab9f55587d4dbea07e138f9a97b9027761
Hlopov Dmitriy <baingshot@gmail.com>
2019-12-06 01:24:30 +0300

    #3 Сделать ручку GET /status (MockMvc Testing)

--- backend/src/main/java/ru/eda/tech/controller/InfoController.java
+++ backend/src/main/java/ru/eda/tech/controller/InfoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class InfoController {
     @GetMapping("/status")
     public ResponseEntity<String> getStatus(){
         return ResponseEntity.ok("");
--- backend/src/test/java/ru/eda/tech/TechApplicationTests.java
+++ backend/src/test/java/ru/eda/tech/TechApplicationTests.java
@@ -1,33 +1,33 @@
 package ru.eda.tech;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.BDDAssertions.then;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 class TechApplicationTests {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate testRestTemplate;
+    private MockMvc mockMvc;
 
     @Test
     public void shouldReturn200WhenSendingRequestGetStatusToController() throws Exception{
-        @SuppressWarnings("rawtypes")
-        ResponseEntity<String> responseEntity = this.testRestTemplate.getForEntity(
-                "http://localhost:" +this.port + "/status", String.class);
-        then(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    this.mockMvc.perform(get("/status"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("")));
     }
-    
+
 }
