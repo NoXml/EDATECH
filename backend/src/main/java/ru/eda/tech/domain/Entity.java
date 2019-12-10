683c29f1993bd1ae45321ec46ff3ff2868b425c1
Hlopov Dmitriy <baingshot@gmail.com>
2019-12-10 12:32:38 +0300

    #9 Добавлено final id в Status, проверка на null в конструкторе Entity

--- backend/src/main/java/ru/eda/tech/domain/Entity.java
+++ backend/src/main/java/ru/eda/tech/domain/Entity.java
@@ -2,15 +2,17 @@ package ru.eda.tech.domain;
 
 import ru.eda.tech.domain.enums.Status;
 
+import java.util.Objects;
+
 public class Entity {
     private Long id;
     private String name;
     private Status status;
 
     public Entity(Long id, String name, Status status) {
-        this.id = id;
-        this.name = name;
-        this.status = status;
+        this.id = Objects.requireNonNull(id);
+        this.name = Objects.requireNonNull(name);
+        this.status = Objects.requireNonNull(status);
     }
 
     public Long getId() {
--- backend/src/main/java/ru/eda/tech/domain/enums/Status.java
+++ backend/src/main/java/ru/eda/tech/domain/enums/Status.java
@@ -2,7 +2,7 @@ package ru.eda.tech.domain.enums;
 
 public enum Status {
     CREATED(0), CHANGED(1), DELETED(2);
-    private Integer id;
+    private final Integer id;
 
     Status(Integer id) {
         this.id = id;
