e4a49ed82e5ba75106130b8f3c70511bc2c9fb61
Evgenii Lapkovskii <admiral96evg@yandex.ru>
2019-12-30 23:58:42 +0300

    #14 requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+
+	useEffect(() => {
+
+		const asyncRequests = async () => {
+			let response;
+
+			response = await fetch('/entity', {
+				method: 'PUT',
+				body: JSON.stringify({name: 'test'})
+			});
+	
+			response = await fetch('/entity', {
+				method: 'GET',
+				body: JSON.stringify({id: 1})
+			});
+	
+			response = await fetch('/entity', {
+				method: 'POST',
+				body: JSON.stringify({id: 1, name: 'test'})
+			});
+	
+			response = await fetch('/entity', {
+				method: 'DELETE',
+				body: JSON.stringify({id: 1})
+			});
+
+			if (response.status !== 200) alert('NOT 200 RESPONSE!');
+		};
+		asyncRequests();
+
+
+	}, []);
+
+  	return (
+    	<div>
+			EDATECH
+    	</div>
+  	);
 }
 
 export default App;
