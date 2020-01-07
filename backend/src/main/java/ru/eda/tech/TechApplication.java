6de01c4585fc7440aa0b8bf42745c79f5724f7b8
Hlopov Dmitriy <baingshot@gmail.com>
2020-01-08 00:09:41 +0300

    #15 Excluding JacksonAutoConfiguration via @SpringBootApplication annotation

--- backend/src/main/java/ru/eda/tech/TechApplication.java
+++ backend/src/main/java/ru/eda/tech/TechApplication.java
@@ -2,8 +2,11 @@ package ru.eda.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        JacksonAutoConfiguration.class
+})
 public class TechApplication {
     public static void main(String[] args) {
         SpringApplication.run(TechApplication.class, args);
