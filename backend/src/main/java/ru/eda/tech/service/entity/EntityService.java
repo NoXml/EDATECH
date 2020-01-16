c615a0af23f62309af4ced92f4322934ccbce9b4
baingshot <baingshot@gmail.com>
2020-01-16 19:53:17 +0300

    #31 Изменения в Entity Controller (#35)
    * #31 Corrections in Entity Controller
    
    * #31 Correct requested changes
    
    * #31 Correct requested changes+
    
    * Correct requested changes(EntityService: read(id), delete(id))

--- backend/src/main/java/ru/eda/tech/service/entity/EntityService.java
+++ backend/src/main/java/ru/eda/tech/service/entity/EntityService.java
@@ -9,13 +9,17 @@ import ru.eda.tech.controller.api.entity.read.EntityReadResponse;
 import ru.eda.tech.controller.api.entity.update.EntityUpdateRequest;
 import ru.eda.tech.controller.api.entity.update.EntityUpdateResponse;
 
+import java.util.List;
+
 public interface EntityService {
 
     EntityCreateResponse create(EntityCreateRequest request);
 
-    EntityReadResponse read(EntityReadRequest request);
+    List<EntityReadResponse> readAll();
+
+    EntityReadResponse read(Long id);
 
     EntityUpdateResponse update(EntityUpdateRequest request);
 
-    EntityDeleteResponse delete(EntityDeleteRequest request);
+    EntityDeleteResponse delete(Long id);
 }
