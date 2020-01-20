05e5a1bc1d0a87690156abd6575990d45f765534
Aleksey Antufev <12766383+AlexAntufiev@users.noreply.github.com>
2020-01-21 01:44:09 +0300

    #36 Добавить учет покрытия тестов при сборке ПР (#37)

---
+++ src/main/java/ru/eda/tech/service/entity/EntityService.java
@@ -0,0 +1,23 @@
+package ru.eda.tech.service.entity;
+
+import ru.eda.tech.controller.api.entity.create.EntityCreateRequest;
+import ru.eda.tech.controller.api.entity.create.EntityCreateResponse;
+import ru.eda.tech.controller.api.entity.delete.EntityDeleteResponse;
+import ru.eda.tech.controller.api.entity.read.EntityReadResponse;
+import ru.eda.tech.controller.api.entity.update.EntityUpdateRequest;
+import ru.eda.tech.controller.api.entity.update.EntityUpdateResponse;
+
+import java.util.List;
+
+public interface EntityService {
+
+    EntityCreateResponse create(EntityCreateRequest request);
+
+    List<EntityReadResponse> readAll();
+
+    EntityReadResponse read(Long id);
+
+    EntityUpdateResponse update(EntityUpdateRequest request);
+
+    EntityDeleteResponse delete(Long id);
+}
---
+++ src/test/java/ru/eda/tech/controller/api/ResponseTest.java
@@ -0,0 +1,34 @@
+package ru.eda.tech.controller.api;
+
+import org.junit.jupiter.api.Test;
+import ru.eda.tech.base.JsonTestContext;
+import ru.eda.tech.controller.api.entity.create.EntityCreateResponse;
+
+class ResponseTest extends JsonTestContext {
+
+    @Test
+    public void whenUseOfAndSerializeUsingJsonGetter_ThenCorrect() {
+        Response<EntityCreateResponse> response = Response
+                        .of(new EntityCreateResponse(1L, "test"))
+                        .status(ResponseStatus.SUCCESS)
+                        .build();
+        shouldSerialize(response, "ResponseSuccess.json");
+    }
+
+    @Test
+    public void whenUseSuccessAndSerializeUsingJsonGetter_ThenCorrect() {
+        Response<EntityCreateResponse> response = Response
+                .success(new EntityCreateResponse(1L, "test"))
+                .build();
+        shouldSerialize(response, "ResponseSuccess.json");
+    }
+
+    @Test
+    public void whenUseFailedAndSerializeUsingJsonGetter_ThenCorrect() {
+        Response<EntityCreateResponse> response = Response
+                .failed(new EntityCreateResponse(1L, "test"))
+                .errorCode("507")
+                .build();
+        shouldSerialize(response, "ResponseFailed.json");
+    }
+}
