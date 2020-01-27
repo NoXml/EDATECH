b7de8e0470edcdf147435c6f4e1850d2289d3027
Hlopov Dmitriy <baingshot@gmail.com>
2020-01-28 01:28:50 +0300

    #20 Corrected requested changes

--- src/main/java/ru/eda/tech/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/ru/eda/tech/controller/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,5 @@
 package ru.eda.tech.controller;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,14 +8,18 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.eda.tech.controller.api.Response;
 
+import java.util.Arrays;
+
 @RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {Throwable.class})
     protected ResponseEntity<Object> handleConflict(RuntimeException e, WebRequest request) {
-        return handleExceptionInternal(e,
-                Response.failed(null)
-                        .errorCode("TechnicalError")
-                        .build(), new HttpHeaders(),
-                HttpStatus.CONFLICT, request);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        Response.failed()
+                                .errorCode("TechnicalError", Arrays.toString(e.getStackTrace()))
+                                .build()
+                );
     }
 }
