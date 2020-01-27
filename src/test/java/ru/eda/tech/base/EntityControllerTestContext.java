50730c8f69c888dc9c06fbc39ceb387e60eddb9f
Hlopov Dmitriy <baingshot@gmail.com>
2020-01-28 00:50:18 +0300

    #29 Correct requested changes

--- src/test/java/ru/eda/tech/base/EntityControllerTestContext.java
+++ src/test/java/ru/eda/tech/base/EntityControllerTestContext.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.io.Resource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.util.StreamUtils;
 import ru.eda.tech.controller.EntityController;
 import ru.eda.tech.service.entity.EntityService;
@@ -12,6 +14,9 @@ import ru.eda.tech.service.entity.EntityService;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({EntityController.class, EntityService.class})
 public abstract class EntityControllerTestContext {
@@ -33,11 +38,33 @@ public abstract class EntityControllerTestContext {
     @Value("classpath:/ru/eda/tech/controller/EntityControllerDeleteById1ResponseContentExpected.json")
     protected Resource deleteById1ResponseContentExpected;
 
-    protected String copyToStringFromResource(Resource resource) throws IOException {
-        String result = null;
-        try (InputStream inputResourceContent = resource.getInputStream()) {
-            result = StreamUtils.copyToString(inputResourceContent, StandardCharsets.UTF_8);
+    protected String getResponseContent(RequestBuilder requestBuilder) {
+        MvcResult mvcResult = null;
+        String responseContent = null;
+        try {
+            mvcResult = mockMvc
+                    .perform(requestBuilder)
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andReturn();
+            responseContent = mvcResult.getResponse().getContentAsString();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    "Error while performing a request with: " + requestBuilder.toString(), e);
         }
-        return result;
+        return responseContent;
+    }
+
+    protected String copyToStringFromResource(Resource resource) {
+        String result = null;
+        try {
+            try (InputStream inputResourceContent = resource.getInputStream()) {
+                result = StreamUtils.copyToString(inputResourceContent, StandardCharsets.UTF_8);
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException(
+                    "Error while copying from: " + resource.getDescription(), e);
+        }
+        return result.replaceAll("\\s++", "");
     }
 }
--- src/test/java/ru/eda/tech/controller/EntityControllerTest.java
+++ src/test/java/ru/eda/tech/controller/EntityControllerTest.java
@@ -1,8 +1,6 @@
 package ru.eda.tech.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.eda.tech.base.EntityControllerTestContext;
 
@@ -11,102 +9,57 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class EntityControllerTest extends EntityControllerTestContext {
 
     @Test
     void create() {
-        Assertions.assertDoesNotThrow(() -> {
-            String requestContent = copyToStringFromResource(createRequestContent);
-            String responseContentExpected = copyToStringFromResource(createResponseContentExpected);
+        String requestContent = copyToStringFromResource(createRequestContent);
+        String responseContentExpected = copyToStringFromResource(createResponseContentExpected);
 
-            MvcResult mvcResult = mockMvc
-                    .perform(post("/entity")
-                            .contentType(APPLICATION_JSON)
-                            .content(requestContent))
-                    .andDo(print())
-                    .andExpect(status().isOk()) //Здесь 404, а не 200
-                    .andReturn();
-            String responseContent = mvcResult.getResponse().getContentAsString();
+        String responseContent = getResponseContent(post("/entity")
+                .contentType(APPLICATION_JSON)
+                .content(requestContent));
 
-            responseContentExpected = responseContentExpected.replaceAll("\\s++", "");
-
-            assertThat(responseContent).isEqualTo(responseContentExpected);
-        });
+        assertThat(responseContent).isEqualTo(responseContentExpected);
     }
 
     @Test
     void readAll() {
-        Assertions.assertDoesNotThrow(() -> {
-            String responseContentExpected = copyToStringFromResource(readAllResponseContentExpected);
+        String responseContentExpected = copyToStringFromResource(readAllResponseContentExpected);
 
-            MvcResult mvcResult = mockMvc.perform(get("/entity"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-            String responseContent = mvcResult.getResponse().getContentAsString();
+        String responseContent = getResponseContent(get("/entity"));
 
-            responseContentExpected = responseContentExpected.replaceAll("\\s++", "");
-
-            assertThat(responseContent).isEqualTo(responseContentExpected);
-        });
+        assertThat(responseContent).isEqualTo(responseContentExpected);
     }
 
     @Test
     void read() {
-        Assertions.assertDoesNotThrow(() -> {
-            String responseContentExpected = copyToStringFromResource(readById1ResponseContentExpected);
+        String responseContentExpected = copyToStringFromResource(readById1ResponseContentExpected);
 
-            MvcResult mvcResult = mockMvc.perform(get("/entity/1"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-            String responseContent = mvcResult.getResponse().getContentAsString();
+        String responseContent = getResponseContent(get("/entity/1"));
 
-            responseContentExpected = responseContentExpected.replaceAll("\\s++", "");
-
-            assertThat(responseContent).isEqualTo(responseContentExpected);
-        });
+        assertThat(responseContent).isEqualTo(responseContentExpected);
     }
 
     @Test
     void update() {
-        Assertions.assertDoesNotThrow(() -> {
-            String requestContent = copyToStringFromResource(updateRequestContent);
-            String responseContentExpected = copyToStringFromResource(updateResponseContentExpected);
+        String requestContent = copyToStringFromResource(updateRequestContent);
+        String responseContentExpected = copyToStringFromResource(updateResponseContentExpected);
 
-            MvcResult mvcResult = mockMvc
-                    .perform(put("/entity")
-                            .contentType(APPLICATION_JSON)
-                            .content(requestContent))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-            String responseContent = mvcResult.getResponse().getContentAsString();
+        String responseContent = getResponseContent(put("/entity")
+                .contentType(APPLICATION_JSON)
+                .content(requestContent));
 
-            responseContentExpected = responseContentExpected.replaceAll("\\s++", "");
-
-            assertThat(responseContent).isEqualTo(responseContentExpected);
-        });
+        assertThat(responseContent).isEqualTo(responseContentExpected);
     }
 
     @Test
     void delete() {
-        Assertions.assertDoesNotThrow(() -> {
-            String responseContentExpected = copyToStringFromResource(deleteById1ResponseContentExpected);
+        String responseContentExpected = copyToStringFromResource(deleteById1ResponseContentExpected);
 
-            MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/entity/1"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-            String responseContent = mvcResult.getResponse().getContentAsString();
+        String responseContent = getResponseContent(MockMvcRequestBuilders.delete("/entity/1"));
 
-            responseContentExpected = responseContentExpected.replaceAll("\\s++", "");
-
-            assertThat(responseContent).isEqualTo(responseContentExpected);
-        });
+        assertThat(responseContent).isEqualTo(responseContentExpected);
     }
 }
