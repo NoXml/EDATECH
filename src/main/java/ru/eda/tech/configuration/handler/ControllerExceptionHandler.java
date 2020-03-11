728570d982f73e90ee2c8be69e19a47eb1ee3118
Dmitrii Khlopov <baingshot@gmail.com>
2020-03-12 02:38:23 +0300

    #45 Added Handling of Validation Exceptions

--- src/main/java/ru/eda/tech/configuration/handler/ControllerExceptionHandler.java
+++ src/main/java/ru/eda/tech/configuration/handler/ControllerExceptionHandler.java
@@ -4,16 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.eda.tech.controller.api.Error;
 import ru.eda.tech.controller.api.ResponseContent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    Map<String, String> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    //ниже ошибка?ловим Throwable, а в метод RuntimeException передаем
     @ExceptionHandler(value = {Throwable.class})
     ResponseEntity<ResponseContent<?>> handleBaseExceptions(RuntimeException e) {
         log.error("Can't finished request", e);
