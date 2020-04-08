59c1449ee51037b885d28f45df2175a6c1ce803d
Dmitrii Khlopov <baingshot@gmail.com>
2020-04-08 22:30:01 +0300

    #52 [BE] Add @CrossOrigin() to getStatus()

--- src/main/java/ru/eda/tech/controller/InfoController.java
+++ src/main/java/ru/eda/tech/controller/InfoController.java
@@ -1,6 +1,7 @@
 package ru.eda.tech.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ public class InfoController {
 
     @ApiOperation(value = "Get backend status state")
     @GetMapping("/status")
+    @CrossOrigin(origins = "*")  // TODO: For development and testing only. Remove for production.
     public String getStatus() {
         return "";
     }
