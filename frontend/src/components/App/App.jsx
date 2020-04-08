909a711bbb9f17db956f6be876a48f30cc4cbb23
Evgenii Lapkovskii <admiral96evg@yandex.ru>
2020-04-09 01:32:06 +0300

    #52 fetch status from backend tithout design

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
-
-
-const handleOnStatusClick = async () => {
-	const response = await fetch('http://localhost:8090/entity', {mode : 'no-cors'});
-	console.log('response', response);
-	const status = await response.json();
-};
+import React, {useState} from 'react';
 
 const App = () => {
+	const [status, setStatus] = useState({
+		isFetching: false,
+		data: null
+	});
+
+	const {isFetching, data} = status;
+
+	const handleOnStatusClick = async () => {
+		setStatus({
+			...status,
+			isFetching: true
+		});
+
+		const response = await fetch('http://localhost:8090/status');
+		const data = await response.text();
+
+		setStatus({
+			...status,
+			isFetching: false,
+			data
+		});
+	};
+
+	const divStyles = {
+		cursor: 'pointer',
+		...(isFetching && {pointerEvents: 'none'})
+	};
+	const formatedData = isFetching ? 'Loading ...' : typeof data === 'string' ? data || 'No status'  : 'Click above to fetch';
+
 	return (
-		<div onClick={handleOnStatusClick}>GET STATUS</div>
+		<div>
+			<div onClick={handleOnStatusClick} style={divStyles}>GET STATUS</div>
+			<h1>{formatedData}</h1>
+		</div>
 	);
 };
 
