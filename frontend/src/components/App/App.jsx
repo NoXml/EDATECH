cfd3c6b80826b6a60ecc41986f1006e6495ea9cc
Evgenii Lapkovskii <admiral96evg@yandex.ru>
2020-04-09 14:38:22 +0300

    #52 ручка /GET status (#53)

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import config from 'config';
 
 const App = () => {
+	const [status, setStatus] = useState({
+		isFetching: false,
+		data: null
+	});
+
+	const {isFetching, data} = status;
+
+	const handleOnStatusClick = async () => {
+		setStatus({
+			...status,
+			isFetching: true
+		});
+
+		const response = await fetch(`${config.backendUrl}/status`);
+		const data = await response.text();
+
+		setStatus({
+			...status,
+			isFetching: false,
+			data
+		});
+	};
+
+	const divStyles = {
+		cursor: 'pointer',
+		...(isFetching && {pointerEvents: 'none'})
+	};
+	const formatedData = isFetching ? 'Loading ...' : typeof data === 'string' ? data || 'No status'  : 'Click above to fetch';
+
 	return (
-		<h1>NORMA</h1>
+		<div>
+			<div onClick={handleOnStatusClick} style={divStyles}>GET STATUS</div>
+			<h1>{formatedData}</h1>
+		</div>
 	);
 };
 
