e64b0c8b55fda25682830e708ec31198c2f71ada
Dmitrii Khlopov <baingshot@gmail.com>
2020-10-10 19:04:57 +0300

    #86 Новый вариант теста

--- src/test/java/ru/eda/tech/TechApplicationTest.java
+++ src/test/java/ru/eda/tech/TechApplicationTest.java
@@ -1,12 +1,21 @@
 package ru.eda.tech;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class TechApplicationTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
-        TechApplication.main(new String[0]);
+        assertThat(applicationContext).isNotNull();
     }
 
 }
